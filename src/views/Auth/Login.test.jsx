3b4df916a5cc82d29378f4c6c96c8d97e30592e5
simonkaine <simonkaine@gmail.com>
2022-01-06 12:30:43 -0800

    Login snapshot test pass.

---
+++ src/views/Auth/Login.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Login from '../Auth/Login';
+import { ProvideAuth } from '../../context/ProvideAuth';
+
+it('should render Login comp + snapshot test', () => {
+  const { container } = render(
+    <MemoryRouter>
+        <ProvideAuth>
+          <Login />
+        </ProvideAuth>
+    </MemoryRouter>
+  );
+
+  expect(container).toMatchSnapshot();
+});
