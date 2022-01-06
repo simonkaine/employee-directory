ddbe4a6ac411ebe1d4281907317b780613bf4c17
simonkaine <simonkaine@gmail.com>
2022-01-05 17:58:18 -0800

    Home route.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './views/Home/Home';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
